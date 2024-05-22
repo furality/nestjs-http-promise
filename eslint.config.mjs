61937e03b496ff6c6cfe70f9bcf33b8ac5a2c54b
Conicaw <conicaw@conicaw.me>
2024-05-22 10:42:25 -0500

    refactor: update eslint
    Update eslint
    
    commit-id:6298dd27

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigDirName: import.meta.dirname,
+        sourceType: 'module',
+      },
+    },
+  },
+);
